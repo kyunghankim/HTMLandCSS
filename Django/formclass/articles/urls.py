7cc2034790799d8e3c46fc40ddf06ff1ffca477f
sarotto rosso <ilikeorange@nate.com>
2020-06-16 15:49:33 +0900

    0615 form

---
+++ Django/formclass/articles/urls.py
@@ -0,0 +1,20 @@
+#from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('<int:pk>/',views.detail, name='detail'),
+    path('<int:pk>/delete/',views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
+]
+
+def new(request):
+    context = {
+        
+    }
+    return render(request, 'articles/new.html',context)
